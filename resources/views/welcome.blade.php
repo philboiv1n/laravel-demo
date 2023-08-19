1b2cba372e42a63774f0eafae666f79d5f4b0796
Phil Boivin <phil@philboivin.com>
2023-08-19 16:24:35 -0400

    .

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel Démo - Philippe Boivin</title>
+        <title>Laravel Demo - Phil Boivin</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
